9d1dda203ca28adec42281d4c356b582a73c3f53
VinniSantos2003 <vinixreduzino@gmail.com>
2024-11-22 11:24:21 -0300

    Funcional com mais de dois pontos

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ float funcao(float x){
 
 int lagrange(){
     int qntPontos(0);
-    float x(0),numeradorRes(0),denominadorRes(0),res(0);
+    float x(0),numeradorRes(1),denominadorRes(1),res(0);
     float **pontos = NULL;
     pontos = new float*[qntPontos];//Vetores para a quantidade de pontos, um vetor para X e outro para Y;
     float *vetorPolinomiosBasicos = new float[qntPontos];//Vetor para guardar o resultado de cada L;
@@ -21,8 +21,6 @@ int lagrange(){
     cin >> x;
     cout << "Insira a quantidade de pontos: ";
     cin >> qntPontos;
-
-
 
     for(int i=0;i<qntPontos;i++){//Inserir pontos
         pontos[i] = new float[2];
@@ -49,31 +47,29 @@ int lagrange(){
     //Dois loops, um guia aonde fica a resposta de *vetorPolinomiosBasicos o outro calcula os X e retorna a resposta quando acabar o loop
 
     for(int i = 0; i < qntPontos;i++){//Guia a resposta do *vetorPolinomiosBasicos
-        for(int j= 0;j <qntPontos;j++){//Calcula o numerador
+        for(int j = 0;j <qntPontos;j++){//Calcula o numerador
             if(j!=i){//Se meu Li for diferente a Xj calculo normalmente
-                numeradorRes += (x-pontos[j][0]);
+                numeradorRes *= (x-pontos[j][0]);
             }
         }
         for(int k = 0;k <qntPontos;k++){//Calcula o denominador
             //O calculo vai ficar Xi - Xk
             if(k!=i){
-                denominadorRes += (pontos[i][0]-pontos[k][0]);
+                denominadorRes *= (pontos[i][0]-pontos[k][0]);
             }
         }
 
         vetorPolinomiosBasicos[i] = (numeradorRes/denominadorRes)*pontos[i][1];
         //Cada casa do vetor terá seu polinomio interpolador multiplicado pelo Y de seu respective ponto
 
-        numeradorRes = 0;
-        denominadorRes = 0;
+        numeradorRes = 1;
+        denominadorRes = 1;
     }
 
     for(int i = 0; i<qntPontos;i++){
         res += vetorPolinomiosBasicos[i];
     }
-
     cout << "Resposta: " << res <<endl;
-
     return 0;
 }
 
